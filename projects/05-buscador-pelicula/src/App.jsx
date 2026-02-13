1960cbe1c6d4d51bce143d5f04dead2f995b2087
Dvku <sebastian.nilom@gmail.com>
2026-02-13 19:50:43 -0300

    Inicio proyecto buscador de peliculas

---
+++ projects/05-buscador-pelicula/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import responseMovies from './mocks/with-results.json'
+import withoutResults from './mocks/no-results.json'
+
+function App() {
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0
+
+
+  return (
+    <div className='page'>
+
+      <header>
+        <h1>Buscador de películas</h1>
+        <form className='form'>
+          <input placeholder='Avengers, Star Wars, The Matrix...' />
+          <button type='submit'>Buscar</button>
+        </form>       
+      </header>
+
+      <main>
+        {
+          hasMovies
+        }
+      </main>
+
+    </div>
+  )
+}
+
+export default App
